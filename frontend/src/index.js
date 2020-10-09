ff523114d52e1008274d1574527b90d1ba3c103c
Danilo Henrique de Canini Romani <daniloh@romagnole.com.br>
2020-10-09 10:24:48 -0300

    🎨 feat: add windmill and tailwind react ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/css/tailwind.output.css'
+import { Windmill } from '@windmill/react-ui';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Windmill usePreferences>
+      <App />
+    </Windmill>
   </React.StrictMode>,
   document.getElementById('root')
 );
