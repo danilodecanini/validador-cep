a2fdf9f1da12fd175fc2573b825d967da21c0252
Danilo de Canini <danilodecanini@gmail.com>
2020-10-12 00:34:46 -0300

    🎉 feat: add API integration with frontend

--- frontend/src/components/CepAdd/index.js
+++ frontend/src/components/CepAdd/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../layout/Header';
 import { Card, CardBody, Input, Label, Button, HelperText } from '@windmill/react-ui';
+import notify from '../../utils/notify';
+import api from '../../services/api';
 
 const CepAdd = () => {
+  const [city, setCity] = useState('');
+  const [cep, setCEP] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  async function handleSubmit(evt){
+    evt.preventDefault();
+    setLoading(true);
+
+    if(!city || !cep){
+      notify('warning', 'Atenção', 'Os campos Cidade e CEP devem ser preenchidos!');
+      setLoading(false);
+      return false;
+    }
+
+    await api.post('/', {
+      city,
+      cep
+    }).then(response => {
+      if(response.data.status === 'success'){
+        notify(response.data.status, 'Sucesso!', response.data.message);
+        setCity('');
+        setCEP('');
+      } else {
+        notify('danger', 'Ops!', response.data.message);
+      }
+    }).catch(err => {
+      if(err.response){
+        notify('danger', 'Ops!', err.response.data.message);
+      } else {
+        notify('danger', 'Ops!', 'Algo deu errado!');
+      }
+    });
+
+    setLoading(false);
+  }
+
   return (
     <>
       <Header />
@@ -11,20 +49,22 @@ const CepAdd = () => {
           <Card>
             <CardBody>
               <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Cadastrar CEP</p>
-              <div className="mb-4">
-                <Label>
-                  <span>Cidade</span>
-                  <Input className="mt-1" />
-                </Label>
-              </div>
-              <div className="mb-4">
-                <Label>
-                  <span>CEP</span>
-                  <Input className="mt-1" />
-                  <HelperText valid={false}>CEP Inválido, verifique o valor digitado</HelperText>
-                </Label>
-              </div>
-              <Button disabled>Cadastrar</Button>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <Label>
+                    <span>Cidade</span>
+                    <Input onChange={e => setCity(e.target.value)} value={city} className="mt-1" />
+                  </Label>
+                </div>
+                <div className="mb-4">
+                  <Label>
+                    <span>CEP</span>
+                    <Input className="mt-1" onChange={e => setCEP(e.target.value)} value={cep}/>
+                    <HelperText>O CEP precisa ser maior que 100.000 e menor que 999.999 <br/>O CEP não pode conter nenhum digito repetitivo alternado em pares</HelperText>
+                  </Label>
+                </div>
+                <Button disabled={isLoading} type="submit">Cadastrar</Button>
+              </form>
             </CardBody>
           </Card>
         </div>
--- frontend/src/components/CepList/index.js
+++ frontend/src/components/CepList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../layout/Header';
 import {
   Card,
@@ -13,9 +13,39 @@ import {
 } from '@windmill/react-ui';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import notify from '../../utils/notify';
 
 
 const CepList = () => {
+  const [ceps, setCeps] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData(){
+      await api.get('/').then(response => {
+        if(response.data) {
+          if(response.data.status === 'success'){
+            setCeps(response.data.data);
+            notify(response.data.status, 'Sucesso!', response.data.message);
+            setLoading(false);
+          } else {
+            notify('danger', 'Ops!', response.data.message);
+          }
+
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        notify('error', 'Ops!', 'Algo deu errado!');
+      });
+
+
+    }
+
+    getData();
+  }, []);
+
   return (
     <>
       <Header />
@@ -27,28 +57,36 @@ const CepList = () => {
               <Link to="/add">
                 <Button className="mb-4" iconLeft={AiOutlinePlus}>Novo CEP</Button>
               </Link>
-              <TableContainer>
-                  <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableCell>Cidade</TableCell>
-                        <TableCell>CEP</TableCell>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      <TableRow>
-                        <TableCell>
-                          <div className="flex items-center text-sm">
-                            <span className="font-semibold ml-2">Mandaguari</span>
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <span className="text-sm">86975-000</span>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+              {!isLoading && (
+                <TableContainer>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableCell>Cidade</TableCell>
+                          <TableCell>CEP</TableCell>
+                          <TableCell>Data Criação</TableCell>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                      {ceps.map(cep => (
+                        <TableRow>
+                          <TableCell>
+                            <div className="flex items-center text-sm">
+                              <span className="font-semibold ml-2">{cep.city}</span>
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            <span className="text-sm">{cep.cep}</span>
+                          </TableCell>
+                          <TableCell>
+                            <span className="text-sm">{new Date(cep.createdAt).toLocaleString()}</span>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+              )}
             </CardBody>
           </Card>
         </div>
